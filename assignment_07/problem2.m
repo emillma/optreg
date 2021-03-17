e24476b84474bface342fe5b9cd07b6bc0166694
Emil Martens <emil.martens@gmail.com>
2021-03-17 23:20:23 +0100

    task3a

--- assignment_07/problem2.m
+++ assignment_07/problem2.m
@@ -22,20 +22,21 @@ A_cl_eigvals = eig(A_cl);
 p_obs_d = 0.5 + 0.03j * [1; -1];
 KF = place(A_d', C', p_obs_d).';
 
-Phi = [A_d-B_d*K B_d*K; zeros(2,2) A_d-KF*C];
+Phi = [A_d - B_d * K B_d * K; zeros(2, 2) A_d - KF * C];
 Phi_eigvals = eig(Phi);
 
 tf = 50;
+x0 = [5, 1]';
+x0_hat = [6, 0]';
+
 x = zeros(2, tf + 1);
-u = zeros(1, tf + 1);
-y = zeros(1, tf + 1);
-x_hat = zeros(2, tf + 1);
-
-x0 = [5, 1]'; 
-x0_hat = [6, 0]'; 
-
 x(:, 1) = x0;
-x_hat(:, 1) = x0_hat;
+
+x_hat = zeros(2, tf + 1);
+kx_hat(:, 1) = x0_hat;
+
+y = zeros(1, tf + 1);
+u = zeros(1, tf + 1);
 
 for t = 1:tf
     u(:, t) = -K * x_hat(:, t);
@@ -44,12 +45,11 @@ for t = 1:tf
     x_hat(:, t + 1) = A_d * x_hat(:, t) + B * u(:, t) + KF * (y(:, t) - C * x_hat(:, t));
 end
 
-
-t_arr = 0:tf;  
+t_arr = 0:tf;
 figure(1);
 
 subplot(2, 1, 1);
-plot(t_arr, x); 
+plot(t_arr, x);
 hold on;
 plot(t_arr, x_hat); hold off;
 legend('x_0', 'x_1', 'x\_hat_0', 'x\_hat_1');
